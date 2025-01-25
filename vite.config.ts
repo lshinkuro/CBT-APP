82363e6ba84b3e4710f5e7ced476dde9563fe03b
Sigit Sasongko <42807174+sgtclothes@users.noreply.github.com>
2025-01-25 10:21:54 +0700

    fix:(Tryout) analyze bug modal

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    define: {
+        require: "undefined",
+    },
     optimizeDeps: {
         exclude: ["lucide-react"],
         esbuildOptions: {
